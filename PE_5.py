970ff340a02de459371e91b66aa63c8c426e831a
gmankowski23 <gmankowski23@bsmschool.org>
2020-10-10 10:05:53 -0500

    Eular Problem

---
+++ PE_5.py
@@ -0,0 +1,11 @@
+x = 0
+y = True
+
+while y == True:
+    x += 2520
+    #print(x)
+    if x % 20 == 0 and x % 19 == 0 and x % 18 == 0 and x % 17 == 0 and x % 16 == 0 and x % 15 == 0 and x % 14 == 0 and x % 13 == 0 and x % 12 == 0 and x % 11 == 0:
+        print(x)
+        y = False
+        
+
